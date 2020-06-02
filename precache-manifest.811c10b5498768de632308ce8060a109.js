ed56129813bdfa1d20ce13f9d4b367dac7f16783
MartaAlbuquerque <martacraveiro.al@gmail.com>
2020-06-02 16:26:51 +0100

    Updates

---
+++ precache-manifest.811c10b5498768de632308ce8060a109.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "207ca342f68ad16508cd2b0bdecbe9ef",
+    "url": "/nearearth/index.html"
+  },
+  {
+    "revision": "f0b072e4ae637c244aa9",
+    "url": "/nearearth/static/css/main.d56d7ff3.chunk.css"
+  },
+  {
+    "revision": "1a8c7d4d45251e949d01",
+    "url": "/nearearth/static/js/2.875eec43.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/nearearth/static/js/2.875eec43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0b072e4ae637c244aa9",
+    "url": "/nearearth/static/js/main.dcc34c1e.chunk.js"
+  },
+  {
+    "revision": "105e9b62cca7f9f19cbd",
+    "url": "/nearearth/static/js/runtime-main.443e0cab.js"
+  }
+]);
